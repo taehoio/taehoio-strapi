a809d3ed14c277426bbd079fde20bf29723e7a91
Taeho Kim <taeho@taeho.io>
2022-01-03 22:43:54 +0900

    Add mysql socket path (#3)

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "34.146.106.115"),
+      socketPath: env("DATABASE_SOCKET_PATH", ""),
+      host: env("DATABASE_HOST", ""),
       port: env("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "taehoio_strapi"),
       user: env("DATABASE_USERNAME", "taehoio_strapi_sa"),
